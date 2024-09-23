fd8ced552e105ac676c43ed6a01a04a15c266ed8
JohnDoeData <JohnDoeData>
2024-09-23 13:22:47 +0000

    - improved pip installation by replacing it with uv

--- 0_main.py
+++ 0_main.py
@@ -1,5 +1,36 @@
 # Databricks notebook source
-# MAGIC  %pip install -r requirements.txt
+# DBTITLE 1,Install Required Packages
+# MAGIC %sh
+# MAGIC uv pip install geopandas fpdf2 --link-mode=copy
+
+# COMMAND ----------
+
+# DBTITLE 1,Import Required Libraries
+import glob
+import json
+import os
+
+from main import main
+
+# COMMAND ----------
+
+# DBTITLE 1,Load JSON Files into a List of Dictionaries
+# Define the path with a wildcard to match all JSON files in the specified directory
+wildcard_path = "/dbfs/mnt/production/dataplatform/silver/sharepoint/innolab/*.json"
+json_files = glob.glob(wildcard_path)
+
+# Load the content of each JSON file as a dictionary and store them in json_list
+json_list: list[dict] = []
+for json_file in json_files:
+    with open(json_file, 'r') as file:
+        json_list.append(json.load(file))
+
+# COMMAND ----------
+
+try:
+    main(json_list[4])
+except Exception as e:
+    print(json_list[4], e)
 
 # COMMAND ----------
 
